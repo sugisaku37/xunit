866990a7b7993f84b85b076e3408690000bcc018
Akinori Sugimura <a.sugimura@akbcsol.com>
2021-07-06 15:34:45 +0900

    testMethodの動的な呼び出し

--- xunit.py
+++ xunit.py
@@ -1,8 +1,10 @@
 class WasRun:
     def __init__(self, name): 
         self.wasRun = None
+        self.name = name
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
     def testMethod(self):
         self.wasRun = 1
 
