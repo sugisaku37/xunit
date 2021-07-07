ab1caa08b14b95d68eb89ba6ec8df16446e8b738
Akinori Sugimura <a.sugimura@akbcsol.com>
2021-07-07 10:09:07 +0900

    TestCaseクラスの作成

--- xunit.py
+++ xunit.py
@@ -1,10 +1,14 @@
-class WasRun:
-    def __init__(self, name): 
-        self.wasRun = None
+class TestCase:
+    def __init__(self, name):
         self.name = name
     def run(self):
         method = getattr(self, self.name)
         method()
+
+class WasRun(TestCase):
+    def __init__(self, name): 
+        self.wasRun = None
+        super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
 
